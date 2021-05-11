327663469d5b5cc03f8808af4fa97a985739953d
FJ <lfjl@live.com>
2021-05-10 22:29:12 -0500

    Basic Lunix commands

---
+++ Linux/linux.bash
@@ -0,0 +1,37 @@
+ls # List content of the folder
+open #.   going to open finder only for MAC or Linux
+start #. for open folders in windows 
+
+ls 'name folder' # shows the content of that folder.
+
+pwd # print out your current location
+cd # moving between folders 
+cd .. # move backwards, one level, one folder
+
+### this command run relative where you are #####
+### CREATE FILES ###
+touch 'file name whit extention' # create o update a file especificated
+touch path/file.txt # will create file
+
+### CREATE FOLDERS ###
+mkdir 'Name-of-folder'... #you can create a lot of folders in the same line separing with blackspace
+
+### DELETE FILE #####
+rm path/file.txt
+rm file.txt ### delete file in the path where you are 
+
+### DELETE FOLDERS ###
+rm -rf folder-name
+rm -rf path/folder-name # Delete folder with giving path
+
+
+
+
+
+
+
+
+
+
+
+
